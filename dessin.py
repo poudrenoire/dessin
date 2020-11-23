5c82873d050d7202d83570794a353ed23a2da876
poudrenoire <74770911+poudrenoire@users.noreply.github.com>
2020-11-23 15:34:31 +0100

    Update dessin.py

--- dessin.py
+++ dessin.py
@@ -1,6 +1,7 @@
 # Génère des dessins aléatoirement
 import turtle
 import random
+import time
 
 screen = turtle.Screen()
 screen.colormode(255)
@@ -13,4 +14,4 @@ col_c = random.randint(0,255)
 # Générateur de dessins
 turtle.pencolor(col_a, col_b, col_c)
 turtle.forward(50)
-sleep()
+time.sleep(20)

97459aebf72c5a167f9c2b308af8d924c82876fc
poudrenoire <74770911+poudrenoire@users.noreply.github.com>
2020-11-23 15:25:22 +0100

    Update dessin.py

--- dessin.py
+++ dessin.py
@@ -3,9 +3,9 @@ import turtle
 import random
 
 # Génération de 3 nombres au hasard pour usage RVB
-col_a = random.random(1.0,255.0)
-col_b = random.randint(1.0,255.0)
-col_c = random.randint(1.0,255.0)
+col_a = random.randrange(0.0,255.0)
+col_b = random.randrange(0.0,255.0)
+col_c = random.randrange(0.0,255.0)
 
 # Générateur de dessins
 turtle.pencolor(col_a, col_b, col_c)

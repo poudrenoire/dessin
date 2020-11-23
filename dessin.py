3d8079a8078abcc6bccaf79a2e1854e521ce7698
poudrenoire <74770911+poudrenoire@users.noreply.github.com>
2020-11-23 15:47:30 +0100

    Update dessin.py

--- dessin.py
+++ dessin.py
@@ -14,6 +14,7 @@ col_c = random.randint(0,255)
 # Générateur de dessins
 for _ in range(250):
   turtle.pencolor(col_a, col_b, col_c)
+  turtle.left(random.randint(0,360))
   turtle.forward(random.randint(0,25))
   turtle.left(random.randint(0,360))
   turtle.pencolor(random.randint(0,255), random.randint(0,255), random.randint(0,255))
@@ -23,4 +24,5 @@ for _ in range(250):
   turtle.circle(random.randint(0,25), random.randint(0,360))
   turtle.pencolor(random.randint(0,255), random.randint(0,255), random.randint(0,255))
   turtle.forward(random.randint(0,25))
+  turtle.reset()
 time.sleep(20)
